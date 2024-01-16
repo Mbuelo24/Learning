4f9132efaf7162418ccdbd0f6592037bf4e95db6
Mbuelo24 <125274324+Mbuelo24@users.noreply.github.com>
2024-01-16 11:40:56 +0200

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,10 @@
+package Learning;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        System.out.println("Welcome");
+        int TheInt = 100;
+        System.out.println(TheInt);
+    }
+}
